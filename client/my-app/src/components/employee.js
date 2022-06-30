25c93b7a4516c30456b2fffcd56d5020a66235d0
IdanYosef1 <idanyosef42@gmail.com>
2022-06-30 23:53:57 +0300

    Add files

--- client/my-app/src/components/employee.js
+++ client/my-app/src/components/employee.js
@@ -1,16 +1,18 @@
-function Employee({emp, difference, divColor, triangle, differenceSalary, lower}) {
+function Employee({ emp, difference, divColor, triangle, differenceSalary, lower }) {
+  const max = (triangle === 'triangleUp') ? emp.Salary : emp.Salary + difference;
+
   return (
     <div className="div-employee">
-        <div className={divColor} style={{height:`calc(260px * ${(emp.Salary / 15000)})`}}>
-            <div className="internalDiv-salary">
-                <span className="empName">{emp.Name}</span> 
-                <div className="div-line"></div> ${emp.Salary.toLocaleString("en-US")} 
-            </div>    
+      <div className={divColor} style={{ height: `calc(150px + 130px * ${(emp.Salary / max)})` }}>
+        <div className="internalDiv-salary">
+          <span className="empName">{emp.Name}</span>
+          <div className="div-line"></div> ${emp.Salary.toLocaleString("en-US")}
         </div>
-        <div className="div-difference">
-          <div className={differenceSalary}> {(lower) ? '-' : ''}${difference.toLocaleString("en-US")}</div>
-          <div className={triangle}></div>
-        </div> 
+      </div>
+      <div className="div-difference">
+        <div className={differenceSalary}> {(lower) ? '-' : ''}${difference.toLocaleString("en-US")}</div>
+        <div className={triangle}></div>
+      </div>
     </div>
   )
 }
